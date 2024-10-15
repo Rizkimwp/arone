9db9c5df37365ee3454b48327ee5c3b1652a02f4
Rizki Maulana <rizki.mwp404@gmail.com>
2024-10-15 12:30:52 +0700

    second commit

--- src/layout/header/index.tsx
+++ src/layout/header/index.tsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const index = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
   return (
     <header className="fixed top-0 left-0 w-full z-50 bg-white border-b border-gray-200 dark:bg-gray-900">
       <nav className="bg-white border-gray-200 py-2.5 dark:bg-gray-900">
@@ -18,11 +23,11 @@ const index = () => {
           <div className="flex items-center lg:order-3">
             {/* <!-- <a href="#" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a> --> */}
             <button
-              data-collapse-toggle="mobile-menu-2"
+              onClick={toggleMobileMenu}
               type="button"
               className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               aria-controls="mobile-menu-2"
-              aria-expanded="false"
+              aria-expanded={isMobileMenuOpen}
             >
               <span className="sr-only">Open main menu</span>
               <svg
@@ -51,16 +56,24 @@ const index = () => {
               </svg>
             </button>
           </div>
+          {/* Drawer */}
           <div
-            className="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-4"
-            id="mobile-menu-2"
+            className={`fixed top-0 right-0 h-full bg-white dark:bg-gray-800 w-64 transform ${
+              isMobileMenuOpen ? "translate-x-0" : "translate-x-full"
+            } transition-transform duration-300 ease-in-out lg:hidden`}
           >
-            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+            <button
+              onClick={toggleMobileMenu}
+              className="p-4 text-gray-500 hover:text-gray-900 dark:hover:text-white"
+            >
+              <span className="sr-only">Close menu</span>
+              &times;
+            </button>
+            <ul className="mt-8 space-y-4 font-medium">
               <li>
                 <Link
                   to="/"
-                  className="block py-2 pl-3 pr-4 text-white bg-purple-700 rounded lg:bg-transparent lg:text-purple-700 lg:p-0 dark:text-white"
-                  aria-current="page"
+                  className="block px-4 py-2 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                 >
                   Beranda
                 </Link>
@@ -68,7 +81,7 @@ const index = () => {
               <li>
                 <Link
                   to="/profile"
-                  className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block px-4 py-2 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                 >
                   Profile
                 </Link>
@@ -76,7 +89,7 @@ const index = () => {
               <li>
                 <Link
                   to="/layanan"
-                  className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block px-4 py-2 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                 >
                   Layanan
                 </Link>
@@ -84,22 +97,29 @@ const index = () => {
               <li>
                 <a
                   href="#"
-                  className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block px-4 py-2 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                 >
                   Artikel
                 </a>
               </li>
-
               <li>
                 <a
                   href="#"
-                  className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block px-4 py-2 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                 >
                   Contact
                 </a>
               </li>
             </ul>
           </div>
+
+          {/* Overlay */}
+          {/* {isMobileMenuOpen && (
+            <div
+              onClick={toggleMobileMenu}
+              className="fixed inset-0 bg-black bg-opacity-50 lg:hidden"
+            ></div>
+          )} */}
         </div>
       </nav>
     </header>
--- src/pages/layanan/hero.tsx
+++ src/pages/layanan/hero.tsx
@@ -3,7 +3,7 @@ import animationData from "../../../public/animation/layanan.json";
 const Layanan = () => {
   return (
     <section className="bg-white dark:bg-gray-900 h-screen">
-      <div className="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-72">
+      <div className="grid max-w-screen-xl px-4 pt-72 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-72">
         <div
           className="mr-auto place-self-center lg:col-span-7"
           data-aos="fade-up"
